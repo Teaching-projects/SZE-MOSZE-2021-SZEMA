772e63cc82743d8ff2747abcacd069626763c9f6
Tivadar K <kamtivi@gmail.com>
2021-06-15 21:22:47 +0200

    Added basic auth component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Login from "./components/auth/Login"
 import { AuthProvider } from "./components/auth/Auth"
 import PrivateRoute from "./components/auth/PrivateRoute"
+import QuestionBaseDashboard from './components/dashboards/QuestionBaseDashboard'
+import CreateTestDashboard from './components/dashboards/CreateTestDashboard'
+import NewsDashboard from './components/dashboards/NewsDashboard'
+import MainDashboard from "./components/dashboards/MainDashboard"
+import TestMakerDashboard from './components/dashboards/TestMakerDashboard'
 
 
 const App = () => {
