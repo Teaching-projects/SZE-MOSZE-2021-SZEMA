7a2d4fb732ee576896766dc4d37872497a913451
tivadark <kamtivi@gmail.com>
2021-06-23 12:57:45 +0200

    add JSDoc workflow and JSDoc default settings

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+/**
+ * 
+ * It's a test string
+ * @type {string}
+ */
+const testString = "TestString";
+
+/**
+ * 
+ * It's a test number
+ * @type {number}
+ */
+ const testNumber = 5;
+
+
 ReactDOM.render(
   <React.StrictMode>
       <App/>
